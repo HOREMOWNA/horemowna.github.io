e88034e38aed722b5d783e435a9293ab5bf02892
Stephane <oklaova@gmail.com>
2020-06-28 09:28:36 -0400

    updated ui

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18358440870a496b00b23261baa38677.js"
+  "/precache-manifest.bf8169eff1d27a45ad294a1e0e329954.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "digitalellc"});
