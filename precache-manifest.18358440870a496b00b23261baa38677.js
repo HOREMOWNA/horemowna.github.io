79b3cb2b44ddb47ba37d195bb006347d33db42e4
Stephane <oklaova@gmail.com>
2020-06-28 01:51:06 -0400

    updated ui

---
+++ precache-manifest.18358440870a496b00b23261baa38677.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "72819cfe72e958aabec9",
+    "url": "/css/app.371bb496.css"
+  },
+  {
+    "revision": "72819cfe72e958aabec9",
+    "url": "/js/app.5cc7ef40.js"
+  },
+  {
+    "revision": "f6317606b73b4a8afaa3",
+    "url": "/js/chunk-vendors.f68e1c69.js"
+  },
+  {
+    "revision": "adcea03489b0ad535589d8ce9935e77e",
+    "url": "/img/offair.adcea034.png"
+  },
+  {
+    "revision": "fc3c9d08ab60f694249724d080576bb5",
+    "url": "/img/onpause.fc3c9d08.png"
+  },
+  {
+    "revision": "d4c33c13ce3c732271d3d2c66aa5e2c0",
+    "url": "/img/onair.d4c33c13.png"
+  },
+  {
+    "revision": "0746d61382691cc3618bd5b06907bfab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba02e59c443cbcc15b9ffbb7d9ae09c0",
+    "url": "/css/horemow.css"
+  },
+  {
+    "revision": "d4ed6b870e7eaf94d56e6a6a7f535a8f",
+    "url": "/css/videojs-overlay.css"
+  },
+  {
+    "revision": "6b37869e44bc7df44fa50662a0e9d028",
+    "url": "/js/adapter.js"
+  },
+  {
+    "revision": "5e3fda35ac545748286f25174e5445f0",
+    "url": "/js/videojs-overlay.min.js"
+  },
+  {
+    "revision": "56a928b0a4bf828ac2c2d9489da8da33",
+    "url": "/js/visualizer"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8a93632cca6ef5d25d73eb104b03119.js"
+  "/precache-manifest.18358440870a496b00b23261baa38677.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "digitalellc"});
