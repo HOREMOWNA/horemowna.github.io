3d9d147d008f4be25c247c8c6361c0f2e7f3d1a9
Stephane <oklaova@gmail.com>
2020-08-02 18:13:40 -0400

    added ssl

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dbcc820315bddc821f41b83c739e774b.js"
+  "/precache-manifest.b7abb3499b2dbabaa9b71e5efdfabe5e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "digitalellc"});
