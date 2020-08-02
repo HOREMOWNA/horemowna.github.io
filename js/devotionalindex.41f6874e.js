cbf263432e85084c560bb58877cec79455ac9b02
Stephane <oklaova@gmail.com>
2020-08-02 05:34:35 -0400

    refreshing with new look

---
+++ js/devotionalindex.41f6874e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["devotionalindex"],{"66fe":function(e,n,t){"use strict";t.r(n);var l=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},o=[],r=t("2877"),u={},i=Object(r["a"])(u,l,o,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=devotionalindex.41f6874e.js.map
